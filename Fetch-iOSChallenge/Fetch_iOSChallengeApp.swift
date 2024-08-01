b38b02671c67449cc7e0f7c12167e02fbff7dfd8
jared <jaredmurray43@icloud.com>
2024-07-31 18:30:34 -0700

    Initial Commit

---
+++ Fetch-iOSChallenge/Fetch_iOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOSChallengeApp.swift
+//  Fetch-iOSChallenge
+//
+//  Created by JaredMurray on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
