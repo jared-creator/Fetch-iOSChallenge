7a4025340844a7d2482c18867a8e1e946d9c57a2
jared <jaredmurray43@icloud.com>
2024-08-05 16:36:49 -0700

    [clean]Get rid of useless test

--- Fetch-iOSChallengeUITests/Fetch_iOSChallengeUITests.swift
+++ Fetch-iOSChallengeUITests/Fetch_iOSChallengeUITests.swift
@@ -13,12 +13,13 @@ final class Fetch_iOSChallengeUITests: XCTestCase {
         continueAfterFailure = false
     }
 
-    func testExample() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        //sometimes the VStack that contains the ingredients and measurements dissappears
-        app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Apple Frangipan Tart"]/*[[".cells",".buttons[\"Apple Frangipan Tart, 52768\"].staticTexts[\"Apple Frangipan Tart\"]",".staticTexts[\"Apple Frangipan Tart\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
-        XCTAssertTrue(app.staticTexts["ingredientStack"].exists)    //hasn't failed through numerous test attempts
-    }
+    //This test was used on a previous ui build/no longer needed
+//    func testExample() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        //sometimes the VStack that contains the ingredients and measurements dissappears
+//        app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Apple Frangipan Tart"]/*[[".cells",".buttons[\"Apple Frangipan Tart, 52768\"].staticTexts[\"Apple Frangipan Tart\"]",".staticTexts[\"Apple Frangipan Tart\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        XCTAssertTrue(app.staticTexts["ingredientStack"].exists)    //hasn't failed through numerous test attempts
+//    }
 }
